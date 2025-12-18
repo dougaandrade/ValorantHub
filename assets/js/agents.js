5252eea3f73d5eeb6eee6747e490271b8a3d1cd6
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2025-12-18 13:03:34 -0300

    feat(agents): update agent display layout and improve styling for better responsiveness

--- assets/js/agents.js
+++ assets/js/agents.js
@@ -16,29 +16,28 @@ function renderMainAgent(agent) {
   const mainContainer = document.querySelector(".agents-content");
 
   mainContainer.innerHTML = `
-  <img src="${agent.image}" alt="${agent.name}" class="agents_main" />
   <div class="agents-container">
-    <span class="name-agents">${agent.name}</span>
-    <p class="biography-agents">${agent.biography}</p>
-    <p class="role-agents">${agent.role}</p>
-    </div>
-    <h3 class="abilities-title">Habilidades</h3>
-    <div class="abilities-container">
-      ${agent.abilities
-        .map(
-          (ability) => `
-        <div class="ability-card">
-          
-          <h4 class="ability-name">${ability.name}</h4>
-          <h5 class="ability-cost">Custo: ${
-            ability.cost || ability.points || "0"
-          }</h5>
-          <p class="ability-description">${ability.description}</p>
-        </div>
+  <span class="name-agents">${agent.name}</span>
+  <p class="biography-agents">${agent.biography}</p>
+  <p class="role-agents">${agent.role}</p>
+  <div class="abilities-container">
+  ${agent.abilities
+    .map(
+      (ability) => `
+    <div class="ability-card">
+    
+    <h4 class="ability-name">${ability.name}</h4>
+    <h5 class="ability-cost">Custo: ${
+      ability.cost || ability.points || "0"
+    }</h5>
+      <p class="ability-description">${ability.description}</p>
+      </div>
       `
-        )
-        .join("")}
+    )
+    .join("")}
     </div>
+    </div>
+    <img src="${agent.image}" alt="${agent.name}" class="agents_main" />
   `;
 }
 
